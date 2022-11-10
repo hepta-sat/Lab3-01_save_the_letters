391aecbd12e387acbae0ddde227060a60e3bcac8
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-10 15:33:49 +0000

    rawserial

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_CDH.h"
+ 
+RawSerial pc(USBTX,USBRX);
+HEPTA_CDH cdh(p5, p6, p7, p8, "sd");
+ 
+int main()
+{
+    pc.printf("Hello world!\r\n");
+    char str[100];
+    mkdir("/sd/mydir", 0777);
+    FILE *fp = fopen("/sd/mydir/test.txt","w");
+    if(fp == NULL) {
+        error("Could not open file for write\r\n");
+    }
+    for(int i=0; i<10; i++)fprintf(fp,"Hello my name is HEPTA!\r\n");
+    fclose(fp);
+    fp = fopen("/sd/mydir/test.txt","r");
+    for(int j = 0; j < 10; j++) {
+        fgets(str,100,fp);
+        pc.puts(str);
+    }
+    fclose(fp);
+    pc.printf("Goodbye!!\r\n");
 }
